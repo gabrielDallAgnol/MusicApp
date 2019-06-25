3fc888e4ad7b3d9b28aa63d5c6935402358d5f6f
Gabriel Schmit Dall'agnol <gabriel@palmsoft.com.br>
2019-06-24 21:02:46 -0300

    Initial Commit

---
+++ MusicApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MusicApp
+//
+//  Created by Palmsoft  on 24/06/19.
+//  Copyright © 2019 Gabriel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
